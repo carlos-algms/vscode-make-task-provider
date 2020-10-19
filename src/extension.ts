b2d6ddd86d6ffe385a2cd5df50d061801465c2b0
Carlos Gomes <carlos.alberto.gomes@zalando.de>
2020-10-19 23:13:49 +0200

    Fix build path

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
