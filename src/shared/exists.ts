5f9bdf525140d4b4d9e4a9bfaa17d0168bddf58c
Carlos Gomes <carlos.alberto.gomes@zalando.de>
2020-10-20 16:44:23 +0200

    Replaced deprecated method fs.exists

--- src/shared/exists.ts
+++ src/shared/exists.ts
@@ -1,9 +1,16 @@
 import fs from 'fs';
 
+/**
+ * Checks if a file exists in the given path
+ */
 export default function exists(file: string): Promise<boolean> {
-  return new Promise<boolean>((resolve, _reject) => {
-    fs.exists(file, (value) => {
-      resolve(value);
+  return new Promise<boolean>((resolve, reject) => {
+    fs.access(file, fs.constants.F_OK, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(true);
+      }
     });
   });
 }
