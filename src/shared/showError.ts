8dfb2fe6050ed13be7293c38082e9f66a813d43e
Carlos Gomes <carlos.alberto.gomes@zalando.de>
2020-10-20 16:51:11 +0200

    Enhanced showError to check if the user dismissed

--- src/shared/showError.ts
+++ src/shared/showError.ts
@@ -3,16 +3,16 @@ import vscode from 'vscode';
 import getOutputChannel from './getOutputChannel';
 import localize from './localize';
 
-export default function showError() {
-  vscode.window
-    .showWarningMessage(
-      localize(
-        'makeTaskDetectError',
-        'Problem finding Make targets. See the output for more information.',
-      ),
-      localize('makeShowOutput', 'Go to output'),
-    )
-    .then(() => {
-      getOutputChannel().show(true);
-    });
+export default async function showError() {
+  const result = await vscode.window.showWarningMessage(
+    localize(
+      'makeTaskDetectError',
+      'Problem finding Make targets. See the output for more information.',
+    ),
+    localize('makeShowOutput', 'Go to output'),
+  );
+
+  if (result !== undefined) {
+    getOutputChannel().show(true);
+  }
 }
