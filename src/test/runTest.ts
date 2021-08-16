a59fab32118db30ff10053b35367f53a30c3ca48
Carlos Gomes <carlos.algms@gmail.com>
2021-08-16 23:00:20 +0200

    Moved to @vscode/test-electron

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import * as path from 'path';
-import { runTests } from 'vscode-test';
+import { runTests } from '@vscode/test-electron';
+import path from 'path';
 
 async function main() {
   try {
