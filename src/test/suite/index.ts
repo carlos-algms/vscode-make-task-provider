ecd0e14e0d5528d37bc15e77375107a96e48d3a8
Carlos Gomes <carlos.algms@gmail.com>
2023-04-07 12:36:10 +0200

    feat: Bump bundled dependencies

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,12 +1,18 @@
-import chai, { expect, should, assert } from 'chai';
+import chai, { assert, expect, should } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import glob from 'glob';
+import { globSync } from 'glob';
 import Mocha from 'mocha';
 import path from 'path';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 
-export function run(): Promise<void> {
+/**
+ * This function can't be async
+ * It would only work by returning a new Promise.
+ * Maybe because VSCode polyfills somehow the global Promise, while async returns the native one 🤷🏼‍♂️
+ * https://github.com/microsoft/vscode-test/blob/addc23e100b744de598220adbbf0761da870eda9/sample/src/test/suite/index.ts#L5
+ */
+export function run(_testRoot: string, done: (error: Error | null, failures?: number) => void) {
   globalThis.expect = expect;
   globalThis.assert = assert;
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -30,29 +36,16 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, '../..');
 
-  return new Promise((resolve, reject) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+  try {
+    const files = globSync('**/**.test.js', { cwd: testsRoot });
 
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            reject(new Error(`${failures} tests failed.`));
-          } else {
-            resolve();
-          }
-        });
-      } catch (error) {
-        console.error(error);
-        reject(error);
-      }
+    mocha.run((failures) => {
+      done(null, failures);
     });
-  });
+  } catch (error) {
+    console.error(error);
+    done(error);
+  }
 }
